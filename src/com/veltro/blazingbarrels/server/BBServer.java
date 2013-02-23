5a7a36bc2d0c6d06f934d115fec0e391000bac32
Jared Heath <silkrawnstudios@gmail.com>
2013-02-22 22:02:17 -0500

    [0.0.1] Initial commit - empty main class

---
+++ src/com/veltro/blazingbarrels/server/BBServer.java
@@ -0,0 +1,19 @@
+package com.veltro.blazingbarrels.server;
+
+/**
+ * Main class - contains {@link #main(String[]) launch method}
+ * 
+ * @author LinearLogic
+ * @version 0.0.1
+ */
+public class BBServer {
+
+	/**
+	 * Program entry point
+	 * 
+	 * @param args ...
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
